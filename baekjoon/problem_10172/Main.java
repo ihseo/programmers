2b7843318a3333f7e3aa0b14a384f9f068277e4f
ihseo <inhyukseo1@gmail.com>
2019-10-16 20:42:00 +0900

    Add Java Solution

---
+++ baekjoon/problem_10172/Main.java
@@ -0,0 +1,11 @@
+package problem_10172;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+}
