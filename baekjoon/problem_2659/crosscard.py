010386580c932ce3925b8c40b0484f40b2f02ed4
ihseo <inhyukseo1@gmail.com>
2019-12-29 16:42:21 +0900

    Add New Problem Solution

--- baekjoon/problem_2659/crosscard.py
+++ baekjoon/problem_2659/crosscard.py
@@ -1,7 +1,9 @@
 def clock_number(num):
     clock_num = num
     for _ in range(3):
-        num = num % 1000 * 10 + num // 1000
+        last_three = num % 1000
+        first_digit = num // 1000
+        num = last_three * 10 + first_digit
         if num < clock_num:
             clock_num = num
     return clock_num
@@ -12,7 +14,7 @@ def solution():
     number = clock_number(number)
     count = 0
 
-    for n in range(1111, 10000):
+    for n in range(1111, number + 1):
         if n == clock_number(n):
             count += 1
         if n == number:
